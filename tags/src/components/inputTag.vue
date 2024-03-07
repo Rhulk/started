a9fe0a9a783fc51e1aaf940c16c53b2697a580bf
rhulk <quique1904@gmail.com>
2024-03-07 23:04:15 +0100

    Revert "Grag and drop inicial"
    This reverts commit e8a78b14be9714dae0a4decbea52a96fabcd785c.

---
+++ tags/src/components/inputTag.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="inputTag">
+        <div class="tags">
+            <div class="tag" v-for="(tag, index) in tags" :key="index">
+                {{ tag }} <button @click="deleteTag(tag)">X</button>
+            </div>
+        </div>
+        <form @submit.prevent="handleSubmit">
+          <input class="input" type="text" v-model="currentValue"
+           @keydown="handlekeydown">
+        </form>
+        <!--<label>Input 2</label><input type="text"
+             v-model="currentValue" @keydown="handlekeydown"> -->
+
+    </div>
+</template>
+
+<script>
+export default {
+    emits: ["onTagsChange"],
+    data() {
+        return {
+            currentValue: "",
+            tags: [],
+        };
+  },
+  methods:{
+
+    deleteTag(tag){
+        this.tags = this.tags.filter(item => item != tag);
+        //this.onTagsChange(this.tags);
+        this.$emit('onTagsChange',this.tags);
+    },
+     
+    handlekeydown(e){
+
+        if (e.key == "Backspace" && this.currentValue == ""){
+            this.tags.pop();
+            //this.onTagsChange(this.tags);
+            this.$emit('onTagsChange',this.tags);
+        }
+    },
+    handleSubmit(){
+        if (this.currentValue != ""){
+            const exist = this.tags.some( item => item == this.currentValue);
+            if (!exist)
+            this.tags.push(this.currentValue);
+            this.currentValue="";
+            //this.onTagsChange(this.tags);
+            this.$emit('onTagsChange',this.tags);
+        }       
+    }
+
+
+  },
+
+};
+</script>
+
+<style>
+.inputTag {
+    display: inline-flex;
+    border: solid 1px #000;
+    border-radius: 3px;
+    height: 43px;
+}
+
+.tags {
+    display: flex;
+    gap: 3px;
+    padding: 5px;
+}
+.tags .tag{
+    display: flex;
+    padding: 5px;
+    border: solid 1px #ccc;
+    gap: 5px;
+    align-content: center;
+    border-radius: 3px;
+}
+
+.inputTag .input{
+    border: none;
+    outline: none;
+    padding: 0 5px;
+}
+.inputTag form{
+    display: inline-flex;
+}
+
+.tag button{
+    background-color: transparent;
+    border: none;
+    border-radius: 3px;
+    cursor: pointer;
+}
+.tag button:hover{
+    background-color: #ccc;
+}
+
+</style>
